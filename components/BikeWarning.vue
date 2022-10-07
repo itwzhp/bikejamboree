1010c2f6179dead26f1bb367fd2aa2b2540ebe6d
lukasz-tomala <lukasz124@gmail.com>
2022-10-07 23:08:14 +0200

    BikeWarning component

---
+++ components/BikeWarning.vue
@@ -0,0 +1,32 @@
+<template>
+  <BikeSection class="bike-warning" background="red">
+    <BikeIcon class="bike-warning__icon" icon="info" color="white" />
+    <BikeParagraph class="bike-warning__paragraph" no-margin>
+      <slot />
+    </BikeParagraph>
+  </BikeSection>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.bike-warning {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 20px;
+  border-radius: 15px;
+  &__icon {
+    transform: scale(1.2)
+  }
+  &__paragraph {
+    color: #fff;
+    margin-left: 15px;
+    font-size: .9em;
+  }
+}
+</style>
