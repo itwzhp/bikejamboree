e739cdf069941964c3da041a9b187336cfba1182
lukasz-tomala <lukasz124@gmail.com>
2022-11-16 01:16:34 +0100

    show current position on homepage

---
+++ components/BikeCurrentState.vue
@@ -0,0 +1,136 @@
+<template>
+  <BikeSection class="bike-current mb-75">
+    <div class="bike-current__map">
+      <BikeMap
+        :stops="stops"
+        :stages="stages"
+        size="small"
+        :current-stage-index="Number(0)"
+        :live-coordinates="liveCoordinates"
+      />
+    </div>
+    <BikeCard class="bike-map-legend">
+      <div>
+        <BikeHeading level="2" no-top-margin size="small">
+          Aktualnie trwa: Etap I
+        </BikeHeading>
+        <table>
+          <tr class="bike-map-legend__table-row">
+            <td
+              class="bike-map-legend__table-cell bike-map-legend__table-cell--first"
+            >
+              <BikeIcon class="bike-map-legend__icon" icon="map-pin" filled />
+            </td>
+            <td class="bike-map-legend__table-cell">
+              Start (12.11.2022 ) - {{ $t('places.astana') }}
+            </td>
+          </tr>
+          <tr class="bike-map-legend__table-row">
+            <td
+              class="bike-map-legend__table-cell bike-map-legend__table-cell--first"
+            >
+              <BikeIcon class="bike-map-legend__icon" icon="map-pin" filled />
+            </td>
+            <td class="bike-map-legend__table-cell">
+              Meta (9.12.2022) - {{ $t('places.aral') }}
+            </td>
+          </tr>
+
+          <tr class="bike-map-legend__table-row">
+            <td
+              class="bike-map-legend__table-cell bike-map-legend__table-cell--first"
+            >
+              <div class="bike-map-legend__route-icon" />
+            </td>
+            <td class="bike-map-legend__table-cell">
+              {{ $t('route.route_of_the_expedition') }}
+            </td>
+          </tr>
+          <tr v-if="liveDateAndTime" class="bike-map-legend__table-row">
+            <td
+              class="bike-map-legend__table-cell bike-map-legend__table-cell--first"
+            >
+              <BikeIcon class="bike-map-legend__live-icon" icon="disc" />
+            </td>
+            <td class="bike-map-legend__table-cell">
+              Aktualne położenie
+              <small class="bike-map-legend__live-date">({{ liveDateAndTime }})</small>
+            </td>
+          </tr>
+        </table>
+      </div>
+    </BikeCard>
+    <article class="bike-current__link">
+      <BikeInternalLink>Zobacz trasę całej wyprawy</BikeInternalLink>
+    </article>
+  </BikeSection>
+</template>
+
+<script>
+import { stopsData, stagesData } from '@/helpers/stages.js'
+
+export default {
+  name: 'BikeCurrentState',
+
+  data() {
+    return {
+      kmlData: ''
+    }
+  },
+  async fetch() {
+    this.kmlData = await fetch('https://inreach.radom.zhp.pl')
+      .then(response => response.json())
+  },
+  computed: {
+    stops() {
+      return stopsData
+    },
+    stages() {
+      return stagesData
+    },
+    liveCoordinates() {
+      return (this.kmlData.lat && this.kmlData.lon) ? [this.kmlData.lat, this.kmlData.lon]: null
+    },
+    liveDate() {
+      return this.kmlData.time
+    },
+    liveDateAndTime() {
+      const date = new Date(this.liveDate)
+      const result = `${date.toLocaleDateString()}, ${date.toLocaleTimeString([], {timeStyle: 'short'})}`
+      return result
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.bike-current {
+  &__map {
+    border-radius: 15px;
+    overflow: hidden;
+    margin-bottom: 30px;
+    min-height: 250px;
+  }
+  &__link {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 30px 30px 0;
+  }
+}
+
+@include screen-size('tablet') {
+  .bike-current {
+    display: flex;
+    align-items: stretch;
+    grid-gap: 30px;
+    &__map {
+      flex: 1 0 300px;
+      margin: 0;
+    }
+    &__link {
+      padding: 30px;
+    }
+  }
+}
+</style>
