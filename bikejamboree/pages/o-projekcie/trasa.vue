fbb64c9050ce93e93dfb5677607b3834abde9de5
lukasz-tomala <lukasz124@gmail.com>
2023-01-10 00:17:47 +0100

    update content

--- bikejamboree/pages/o-projekcie/trasa.vue
+++ bikejamboree/pages/o-projekcie/trasa.vue
@@ -1,9 +1,17 @@
 <template>
   <main>
-    <BikeHeading centered level="2">
-      {{ $t('route.route') }}
-    </BikeHeading>
+    <BikeSection>
+      <BikeHeading centered level="2">
+        {{ $t('route.route') }}
+      </BikeHeading>
+      <BikeParagraph v-if="$i18n.locale === 'pl'">
+        Trasa sztafety "JednoŚladami Andersa" liczy około 15 000 km i jest podzielona na 15 etapów - 14 rowerowych i jeden morski. W Mediolanie trasa rozdziela sie na dwie odnogi ("West" i "East"), przez co w tym samym czasie będą odbywały się dwa etapy sztafety.
+        <BikeParagraph />
+      </bikeparagraph>
+    </BikeSection>
     <BikeStages class="mb-75" />
+    </bikeparagraph>
+    </bikeparagraph>
   </main>
 </template>
 
